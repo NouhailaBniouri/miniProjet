1dcee8d698734e0b9003b786a29cd4404ff1cb04
NouhailaBniouri <103757953+NouhailaBniouri@users.noreply.github.com>
2022-04-20 17:17:54 +0000

    Create Bibliothecaire.java

---
+++ code/Bibliothecaire.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * Un bibliothecaire peut gerer l'ensemble des livres il peut les lister et/ou enregistrerEmprut
+ */
+public class Bibliothecaire {
+
+	private int idBib;
+	private String nom;
+	private String prenom;
+
+	public ArrayList<Livre> listrer() {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	public void enregistrerEmprunt() {
+		throw new UnsupportedOperationException();
+	}
+
+}
