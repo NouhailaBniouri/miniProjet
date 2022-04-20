29f849b12a3492999b43f06b4cbb58ada0a99ee7
NouhailaBniouri <103757953+NouhailaBniouri@users.noreply.github.com>
2022-04-20 17:17:58 +0000

    Create Emprunt.java

---
+++ code/Emprunt.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+/**
+ * Pendant l’emprunt le nom de l'emprunteur, la date de prêt, le id de livre sont notés dans le système.Lorsque le livre n’est pas disponible l’emprunt n’est pas possible
+ */
+public class Emprunt {
+
+	private Date date_Emprunt;
+	private Date date_retour;
+
+}
